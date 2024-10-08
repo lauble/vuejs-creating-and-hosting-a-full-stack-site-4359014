f8a0e2fe33c8a7c0d1a99dd0b138cc0096fdddd5
Lauren <lauren.noble3@gmail.com>
2024-10-08 16:00:22 -0400

    complete front-end

---
+++ front-end/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar />
+  <div class="page-wrap">
+    <RouterView></RouterView>
+  </div>
+</template>
+
+<script>
+import NavBar from '@/components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+    NavBar,
+  }
+}
+</script>
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length > 0">
+    <ShoppingCartList :cartItems="cartItems" />
+    <button class="checkout-button">Proceed to checkout</button>
+  </div>
+  <div v-if="cartItems.length === 0">
+    You currently don't have any items in your cart.
+  </div>
+</template>
+
+<script>
+import ShoppingCartList from '../components/ShoppingCartList.vue'
+import { cartItems } from '../temp-data'
+export default {
+  name: "ShoppingCartPage",
+  data() {
+    return {
+      cartItems,
+    }
+  },
+  components: {
+    ShoppingCartList,
+  }
+}
+</script>
