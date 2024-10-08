edf292f55c3c7bfcc4bb8d9cdcb7267a91dc8915
Lauren <lauren.noble3@gmail.com>
2024-10-08 16:41:58 -0400

    add post and delete requests for shopping cart

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,9 +1,17 @@
 import express from 'express';
-import { cartItems, products } from './temp-data';
+import {
+  cartItems as cartItemsRaw,
+  products as productsRaw,
+} from './temp-data';
+
+let cartItems = cartItemsRaw;
+let products = productsRaw;
 
 const app = express();
+app.use(express.json());
 const PORT = 8080;
 
+// GET requests
 app.get('/products', (req, res) => {
   res.json(products);
 });
@@ -16,6 +24,21 @@ app.get('/products/:productId', (req, res) => {
   const productId = req.params.productId;
   const product = products.find((product) => product.id === productId);
   res.json(product);
+});
+
+// POST requests
+app.post('/cart', (req, res) => {
+  const productId = req.body.id;
+  const product = products.find((product) => product.id === productId);
+  cartItems.push(product);
+  res.json(cartItems);
+});
+
+// DELETE requests
+app.delete('/cart/:productId', (req, res) => {
+  const productId = req.params.productId;
+  cartItems = cartItems.filter((product) => product.id !== productId);
+  res.json(cartItems);
 });
 
 app.listen(PORT, () => {
